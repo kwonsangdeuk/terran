2394ba592f961062b4a7a732a1a73fb30ab5c684
ksdmi <ksdmi@host.docker.internal>
2023-12-29 10:27:00 +0900

    second test

---
+++ src/sang/dijt.java
@@ -0,0 +1,19 @@
+package sang;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class dijt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Integer> ll = new ArrayList<>();
+		
+		
+		System.out.println("test");
+		
+		
+	}
+
+}
